f08ba87bfc04797010329b318980eea0f94cdf7c
Gulzar <gulzarhome54@gmail.com>
2017-08-15 22:11:42 +0530

    Story model added

---
+++ app/src/main/java/com/eventer/app/model/Story.java
@@ -0,0 +1,16 @@
+package com.eventer.app.model;
+
+import org.parceler.Parcel;
+
+/**
+ * Created by gulza on 8/15/2017.
+ */
+@Parcel
+public class Story {
+    public String storyName;
+    public String storyUrl;
+    public String storyKey;
+    public String storyDescription;
+    public long timestamp;
+    public Story(){}
+}
