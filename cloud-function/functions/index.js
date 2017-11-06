4e514a42948fd04defbb72265f637abc58dbb495
Gulzar Ahmed <gulzarhome54@gmail.com>
2017-11-06 21:01:36 +0530

    Cloud function code to restrict registration

---
+++ cloud-function/functions/index.js
@@ -0,0 +1,94 @@
+'use strict';
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+//
+
+const secondYearPrefix = 16;
+exports.moderator = functions.database
+    .ref('/events/{eventId}/registers/{userId}').onCreate(event => {
+      const user = event.data.val();
+      const regno = user.regno;
+      const root = event.data.ref.root;
+      const eventid = event.params.eventId;
+      const userid = event.params.userId;
+      const yearprefix = parseInt(regno.substring(0,2));
+      var flag = 1;
+      //Can put more restriction create an array of events
+
+      if(yearprefix != secondYearPrefix)
+      {
+        flag = 0;
+        return event.data.adminRef.remove().then(function() {
+          console.log("Remove succeeded.");
+          console.log("Event id ",eventid);
+          //Now decrement the counter
+          return root.child(`/events/${eventid}/registerCount`).once('value').then(snap =>{
+              const regCount = parseInt(snap.val())-1;
+              console.log("New count value ",regCount);
+              return root.child(`/events/${eventid}`).update({
+                registerCount: regCount,
+              }).then(function(){
+                /*
+                Sends a notification Informing his registration is not successful
+                -First Get the event name !
+                -Get the FCM token from fcm-token/{userid}
+                -Send Notification
+                */
+               return root.child(`/events/${eventid}/title`).once('value').then(snap =>{
+                 const eventName = snap.val();
+                 return root.child(`/fcm-tokens/${userid}/token`).once('value').then(snap =>{
+                 //Get token Value  
+                 const token = snap.val();
+                 console.log('FCM token ',token);
+                 //Generate Payload
+                 const payload = {
+                  data: {
+                      textMessage:"True",
+                      notificationIcon: "https://safebytes.com/wp-content/uploads/2017/03/warning_exclamation.png",
+                      message: "Registration open only for 2nd years",
+                      title: eventName+" Reg Failed"
+                    }
+                };
+                 //Send Notification
+                return admin.messaging().sendToDevice(token, payload)
+                 .then(function (response) {
+                     console.log("Successfully sent message:", response);
+                 })
+                 .catch(function (error) {
+                     console.log("Error sending message:", error);
+                  });   
+                 })
+               })  
+              })
+            })  
+            })
+        .catch(function(error) {
+          console.log("Remove failed: " + error.message)
+        });
+      }
+
+
+      // //add event details
+      // if( flag == 1)
+      //   return root.child(`/my-events/${userid}/${eventid}`).set(true);
+      
+});
+
+// exports.unregisterFromEvent = functions.database.ref('/events/{eventId}/registers/{userId}')
+// .onDelete(event => {
+//   const user = event.data.previous.val();
+//   const regno = user.regno;
+//   const root = event.data.ref.root;
+//   const eventid = event.params.eventId;
+//   const userid = event.params.userId;
+//   var adaRef = admin.database().ref(`/my-events/${userid}/${eventid}`);
+//   return adaRef.remove()
+//   .then(function() {
+//     console.log("Remove succeeded.")
+//   })
+//   .catch(function(error) {
+//     console.log("Remove failed: " + error.message)
+//   });
+
+// });
